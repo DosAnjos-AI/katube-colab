88f9f2cbfb966f259ead52c8c5d08a7e88cc4746
Marcos dos Anjos <viniciusdosanjos@discente.ufg.br>
2025-10-13 10:41:41 -0300

    atualizando init

--- downloaders/__init__.py
+++ downloaders/__init__.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 """
 Downloaders Package - Katube Colab
-Módulos de download e gerenciamento de armazenamento
+Modulos de download e gerenciamento de armazenamento
 """
 
 from .youtube_downloader import YouTubeDownloader
 from .drive_manager import DriveManager
+from .database_manager import DatabaseManager
 
-__all__ = ['YouTubeDownloader', 'DriveManager']
+__all__ = ['YouTubeDownloader', 'DriveManager', 'DatabaseManager']
 __version__ = '1.0.0'
